1dd027a8aa7a90b56d44176aba2a1b2873c6b759
OwDevon <57859896+OwDevon@users.noreply.github.com>
2024-05-20 22:39:19 +0200

    Add files via upload

---
+++ exo_books/exo_books/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// const CONFIG = [
+//     'database' => [
+//         'host' => 'db.3wa.io',
+//         'port' => 3306,
+//         'charset' => 'utf8',
+//         'dbname' => 'matthieuf_distorsion_bst30',
+//         'dbuser' => 'matthieuf',
+//         'dbpassword' => '3f35b2538347916ca4c83838ce211375'
+//     ]
+// ];
+
+const DB_HOST = 'db.3wa.io';
+const DB_PORT = 3306;
+const DB_CHARSET = 'utf8';
+const DB_NAME = 'devonlegaleri_books';
+const DB_USER = 'devonlegaleri';
+const DB_PASSWORD = '50d4a8160360bcfa993d66377e32e475';
